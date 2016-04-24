2bf6c71ede4f8dd1d356707fad1188e301fa9af8
Anthony Sandoval <retrovirus546@gmail.com>
2016-04-23 22:21:36 -0700

    Added some stuff

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -7,19 +7,24 @@ $(function () {
         
         var postsCollection = Backendless.Persistence.of(Posts).find();
         
-        var wrapper = {
-            posts: postsCollection.data
-        };
         
-        Handlebars.registerHelper('format', function (time){
-            return moment(time).format("dddd, MMMM Do YYYY");
-        });
              
-        var blogScript = $("#blogs-template").html();
-        var blogTemplate = Handlebars.compile(blogScript);
-        var blogHTML = blogTemplate(wrapper);
+        var loginScript = $("#login-template").html();
+        var loginTemplate = Handlebars.compile(loginScript);
         
-        $('.main-container').html(blogHTML);
+        
+        $('.main-container').html(loginTemplate);
+        
+        $(document).on('submit', '.form-signin', function(event){
+            event.preventDefault();
+            
+            var data = $(this).serializeArray(),
+                email = data[0].value,
+                password = data[1].value;
+                
+                Backendless.UserService.login(email, password, true, new Backendless.Async(userLoggedIn, gotError));
+            
+        });
 });
 
 function Posts(args){
@@ -27,6 +32,15 @@ function Posts(args){
     this.title = args.title || "";
 //    this.content = args.content || "";
 //    this.authorEmail = args.authorEmail || "";
+}
+
+function userLoggedIn() {
+    console.log("user succsessfully logged in");
+}
+
+function gotError(error){
+    console.log("Error message-" + error.message);
+    console.log("Error code - " + error.code);
 }
 
   $('.dropdown-button').dropdown({
